efc75525dd172840acec8e4e334900ef3c2afffd
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-05-20 16:46:37 -0400

    remove class DataProcessor

--- dev/processing/processing.py
+++ dev/processing/processing.py
@@ -1,51 +1,5 @@
 import pandas as pd
 
-class DataProcessor:
-    """Base processor to be used for all preparation.
-    """
-
-    def __init__(self, filepath: str, nrows=None, dtype=None):
-        """How to handle error if file does not exist?
-        How to take all arguments as method pd.read_csv?
-        """
-        self.filepath = filepath
-        self.data = pd.read_csv(filepath, nrows=nrows, dtype=dtype)
-
-    def get_data(self):
-        return self.data
-
-    def save_csv(self, filepath: str):
-        """Saves processed data."""
-        self.data.to_csv(filepath)
-
-    def get_columns(self):
-        return self.data.columns
-
-    def rename_columns(self, old_cols: list, new_cols: list):
-        self.data = self.data.rename(columns=dict(zip(old_cols, new_cols)))
-
-    def drop_columns(self, colnames: list):
-        self.data = self.data.drop(colnames, axis=1)
-
-    def merge(self, newdata, *args, **kwargs):
-        self.data = pd.merge(self.data, newdata.data, *args, **kwargs)
-
-    def create_target(self, condition):
-        self.target = self.data[condition]
-
-
-class FeatureSelector:
-    def __init__(self, data: pd.DataFrame):
-        self.data = data
-        self.columns = self.data.columns
-
-    def get_columns(self):
-        return self.columns
-
-    def remove_columns(self, removecols: list):
-        newcols = list(set(self.columns) - set(removecols))
-        self.columns = newcols
-
 
 def merge_one_airport(flights, airports, location: str):
     DROPCOLS = ["IATA_CODE", "COUNTRY"]
@@ -62,6 +16,4 @@ def merge_one_airport(flights, airports, location: str):
 def merge_two_airports(flights, airports, locations: list):
     for location in locations:
         flights = merge_one_airport(flights, airports, location)
-
-
 
