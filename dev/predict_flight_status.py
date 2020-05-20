0fe4538b642e7bf07fb39022d924153b2051d019
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-05-20 16:45:18 -0400

    use pd.read_csv instead of class DataProcessor

--- dev/predict_flight_status.py
+++ dev/predict_flight_status.py
@@ -1,10 +1,14 @@
-import code2 as c
+from os.path import join
+import pandas as pd
 
-coltypes = {"ORIGIN_AIRPORT": object, "DESTINATION_AIRPORT": object}
-flights = c.DataProcessor("flights.csv", 100, dtype=coltypes)
-airports = c.DataProcessor("airports.csv")
-holidays = c.DataProcessor("2015_Public_Holidays.csv")
-# add new variables based on departure and arrival airports
+COLTYPES = {"ORIGIN_AIRPORT": object, "DESTINATION_AIRPORT": object}
+
+flights = pd.read_csv("../data/raw/flights.csv", 100, dtype=COLTYPES)
+airports = pd.read_csv("../data/raw/airports.csv")
+holidays = pd.read_csv("../data/raw/2015_Public_Holidays.csv")
+
+"""Merge files to append more features
+"""
 # TODO: write pytest to check if any airport can't be matched
 c.merge_two_airports(flights, airports, ["ORIGIN", "DESTINATION"])
 # add holiday days
