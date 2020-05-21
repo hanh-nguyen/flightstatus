8d9abb65f1d897b3118554c61512dc7979d45fe4
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-05-20 23:00:29 -0400

    add new features related with airports and airlines

--- dev/predict_flight_status.py
+++ dev/predict_flight_status.py
@@ -1,8 +1,10 @@
 import pandas as pd
-from processing import merge_two_airports, categorize_multiple
+from processing import *
 
 COLTYPES = {"ORIGIN_AIRPORT": object, "DESTINATION_AIRPORT": object}
-CAT_FEATURES = ['AIRLINE', 'DESTINATION_AIRPORT', 'ORIGIN_AIRPORT']
+CAT_FEATURES = ['AIRLINE', 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT']
+AIRPORTS = ['ORIGIN_AIRPORT', 'DESTINATION_AIRPORT']
+COORDINATES = ['LONGITUDE', 'LATITUDE']
 
 flights = pd.read_csv("./data/raw/flights.csv", nrows=100, dtype=COLTYPES)
 airports = pd.read_csv("./data/raw/airports.csv")
@@ -16,8 +18,36 @@ check if the # columns & rows are expected
 flights = merge_two_airports(flights, airports, ["ORIGIN", "DESTINATION"])
 flights = flights.merge(holidays, how="left", on=["MONTH", "DAY"])
 
-"""Create new features
+"""Create categorical features
 """
 flights = categorize_multiple(flights, CAT_FEATURES)
+
+"""Create airport-related features:
+- international airport flag
+- airport size estimate based on flight counts
+"""
+airport_size = flights[AIRPORTS[0]].value_counts(
+) + flights[AIRPORTS[1]].value_counts()
+
+for airport in AIRPORTS:
+    airport_intl = airport+"_INTL"
+    flights[airport_intl] = flights[airport].apply(is_internationl)
+    flights = join_aggregates(flights, airport, airport_size, airport+"_SIZE")
+
+"""Create airline-related features:
+- airline size estimate based on flight counts
+"""
+airline_size = flights['AIRLINE'].value_counts()
+flights = join_aggregates(flights, 'AIRLINE', airline_size, "AIRLINE_SIZE")
+
+"""Create flight-related features:
+- flight distance
+"""
+for coordinate in COORDINATES:
+    coordinate_dif =
+    flights[coordinate+"_DIF"] = flights[coordinate +
+                                         '_ORIGIN'] - flights[coordinate+'_DESTINATION']
+
+
 print(flights.columns)
 print(flights.shape)
