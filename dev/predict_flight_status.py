67272c904af6bd9124b2b626b0aef0b5556232dc
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-05-20 21:43:43 -0400

    revise files

--- dev/predict_flight_status.py
+++ dev/predict_flight_status.py
@@ -1,17 +1,16 @@
-from os.path import join
 import pandas as pd
+from processing import merge_two_airports
 
 COLTYPES = {"ORIGIN_AIRPORT": object, "DESTINATION_AIRPORT": object}
 
-flights = pd.read_csv("../data/raw/flights.csv", 100, dtype=COLTYPES)
-airports = pd.read_csv("../data/raw/airports.csv")
-holidays = pd.read_csv("../data/raw/2015_Public_Holidays.csv")
+flights = pd.read_csv("./data/raw/flights.csv", nrows=100, dtype=COLTYPES)
+airports = pd.read_csv("./data/raw/airports.csv")
+holidays = pd.read_csv("./data/raw/2015_Public_Holidays.csv")
 
 """Merge files to append more features
+TODO: write pytest to check if any airport can't be matched
 """
-# TODO: write pytest to check if any airport can't be matched
-c.merge_two_airports(flights, airports, ["ORIGIN", "DESTINATION"])
-# add holiday days
-flights.merge(holidays, how="left", on=["MONTH", "DAY"])
-print(flights.get_columns())
-print(len(flights.get_columns()))
+flights = merge_two_airports(flights, airports, ["ORIGIN", "DESTINATION"])
+flights = flights.merge(holidays, how="left", on=["MONTH", "DAY"])
+print(flights.columns)
+print(len(flights))
--- dev/processing/processing.py
+++ dev/processing/processing.py
@@ -5,15 +5,15 @@ def merge_one_airport(flights, airports, location: str):
     DROPCOLS = ["IATA_CODE", "COUNTRY"]
     OLDCOLS = ["AIRPORT", "CITY", "STATE", "LATITUDE", "LONGITUDE"]
     NEWCOLS = [old + "_" + location for old in OLDCOLS]
-    flights.merge(
+    flights = flights.merge(
         airports, how="left", left_on=location + "_AIRPORT", right_on="IATA_CODE"
     )
-    flights.drop_columns(DROPCOLS)
-    flights.rename_columns(OLDCOLS, NEWCOLS)
+    flights.drop(DROPCOLS, axis=1, inplace=True)
+    flights.rename(columns=dict(zip(OLDCOLS, NEWCOLS)), inplace=True)
     return flights
 
 
 def merge_two_airports(flights, airports, locations: list):
     for location in locations:
         flights = merge_one_airport(flights, airports, location)
-
+    return flights
