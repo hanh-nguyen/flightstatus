26c0ee828f215ead773876a63b66af35ddba308a
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-05-20 22:01:29 -0400

    add function to categorize features

--- dev/predict_flight_status.py
+++ dev/predict_flight_status.py
@@ -1,16 +1,23 @@
 import pandas as pd
-from processing import merge_two_airports
+from processing import merge_two_airports, categorize_multiple
 
 COLTYPES = {"ORIGIN_AIRPORT": object, "DESTINATION_AIRPORT": object}
+CAT_FEATURES = ['AIRLINE', 'DESTINATION_AIRPORT', 'ORIGIN_AIRPORT']
 
 flights = pd.read_csv("./data/raw/flights.csv", nrows=100, dtype=COLTYPES)
 airports = pd.read_csv("./data/raw/airports.csv")
 holidays = pd.read_csv("./data/raw/2015_Public_Holidays.csv")
 
-"""Merge files to append more features
-TODO: write pytest to check if any airport can't be matched
+"""Merge data files
+TODO: 
+write pytest to check if any airport can't be matched (missing in city, state, ...)
+check if the # columns & rows are expected
 """
 flights = merge_two_airports(flights, airports, ["ORIGIN", "DESTINATION"])
 flights = flights.merge(holidays, how="left", on=["MONTH", "DAY"])
+
+"""Create new features
+"""
+flights = categorize_multiple(flights, CAT_FEATURES)
 print(flights.columns)
-print(len(flights))
+print(flights.shape)
--- dev/processing/processing.py
+++ dev/processing/processing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def merge_one_airport(flights, airports, location: str):
+def merge_one_airport(flights: pd.DataFrame, airports: pd.DataFrame, location: str):
     DROPCOLS = ["IATA_CODE", "COUNTRY"]
     OLDCOLS = ["AIRPORT", "CITY", "STATE", "LATITUDE", "LONGITUDE"]
     NEWCOLS = [old + "_" + location for old in OLDCOLS]
@@ -17,3 +17,14 @@ def merge_two_airports(flights, airports, locations: list):
     for location in locations:
         flights = merge_one_airport(flights, airports, location)
     return flights
+
+
+def categorize(data: pd.DataFrame, feature: str):
+    data[feature+"_CAT"] = data[feature].astype("category").cat.codes + 1
+    return data
+
+
+def categorize_multiple(data, features: list):
+    for feature in features:
+        data = categorize(data, feature)
+    return data
