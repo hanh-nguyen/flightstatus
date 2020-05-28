d856a63f0e8ce401b2b78f04e8886cfb7b5f271c
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-05-28 10:42:02 -0400

    add tests for merge_one_airport func

--- test/datapreparation/test_processing.py
+++ test/datapreparation/test_processing.py
@@ -8,10 +8,29 @@ os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 COLTYPES = {"ORIGIN_AIRPORT": object, "DESTINATION_AIRPORT": object}
 
 
-def test_merge_one_airport():
-    raw_data = pd.read_csv("./data/raw/flights.csv", nrows=100, dtype=COLTYPES)
-    airports = pd.read_csv("./data/raw/airports.csv")
-    merged_data = merge_one_airport(raw_data, airports, "ORIGIN")
+class test_merge_one_airport():
+    raw_data = pd.read_csv("./data/dev/flights.csv",
+                           nrows=100, dtype=COLTYPES)
+    airports = pd.read_csv("./data/dev/airports.csv")
+
+    def test_correct_row(self):
+        merged_data = merge_one_airport(self.raw_data, self.airports, "ORIGIN")
+        assert len(merged_data) == len(self.raw_data)
+
+    def test_correct_column(self):
+        merged_data = merge_one_airport(self.raw_data, self.airports, "ORIGIN")
+        assert len(merged_data.columns) == len(
+            self.raw_data.columns) + len(self.airports.columns) - 2
+
+    def test_keyerror_flight(self):
+        with pytest.raises(KeyError):
+            merge_one_airport(self.raw_data.drop(
+                "ORIGIN_AIRPORT", axis=1), self.airports, "ORIGIN")
+
+    def test_keyerror_airport(self):
+        with pytest.raises(KeyError):
+            merge_one_airport(self.raw_data, self.airports.drop(
+                "IATA_CODE", axis=1), "ORIGIN")
 
 
 class TestIsInternational:
