193d4fed9208d54915f17d8f81c39d2415cdf692
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-05-29 19:30:53 -0400

    updated

--- dev/datapreparation/processing.py
+++ dev/datapreparation/processing.py
@@ -4,7 +4,7 @@ from sklearn.feature_selection import VarianceThreshold
 
 def merge_one_airport(flights: pd.DataFrame, airports: pd.DataFrame, location: str):
     DROPCOLS = ["IATA_CODE"]
-    OLDCOLS = ["AIRPORT", "CITY", "STATE", "LATITUDE", "LONGITUDE"]
+    OLDCOLS = ["AIRPORT", "CITY", "STATE", "COUNTRY", "LATITUDE", "LONGITUDE"]
     NEWCOLS = [old + "_" + location for old in OLDCOLS]
     flights = flights.merge(
         airports, how="left", left_on=location + "_AIRPORT", right_on="IATA_CODE"
@@ -39,14 +39,19 @@ def join_aggregates(data, col, aggregates, agg_name):
     return pd.merge(data, aggregates.to_frame(name=agg_name), how='left', left_on=col, right_index=True)
 
 
-def define_target(data, col, threshold):
-    return (data[col] > threshold | data[col].isnull())*1
+def define_target(data, target_col, threshold):
+    return (data[target_col] > threshold | data[target_col].isnull())*1
 
 
 def remove_constant_column(data):
     selector = VarianceThreshold()
-    selector.fit(dropna.drop(['target', 'experiancustomerid_orig'], axis=1))
-    variance_sel = pd.DataFrame({'feature': dropna.drop(['target', 'experiancustomerid_orig'], axis=1).columns,
+    selector.fit(data)
+    variance_sel = pd.DataFrame({'feature': data.drop(['target', 'experiancustomerid_orig'], axis=1).columns,
                                  'selected': selector.get_support()})
     cols = variance_sel[variance_sel['selected']]['feature'].tolist()
     len(cols)
+
+
+def get_col_with_null(data):
+    table_null = data.isnull().sum()
+    return table_null[table_null > 0].index.tolist()
--- dev/predict_flight_status.py
+++ dev/predict_flight_status.py
@@ -1,31 +1,42 @@
 import pandas as pd
 from datapreparation import *
 import os
+from sklearn.feature_selection import VarianceThreshold
+from pandas.api.types import is_string_dtype, is_numeric_dtype
 
 os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 COLTYPES = {"ORIGIN_AIRPORT": object, "DESTINATION_AIRPORT": object}
-CAT_FEATURES = ['AIRLINE', 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT']
 AIRPORTS = ['ORIGIN_AIRPORT', 'DESTINATION_AIRPORT']
 AIRPORT_NAMES = ['AIRPORT_ORIGIN', 'AIRPORT_DESTINATION']
 COORDINATES = ['LONGITUDE', 'LATITUDE']
-
+UNAVAILABLE = ['ARRIVAL_TIME', 'ARRIVAL_DELAY', 'DIVERTED', 'CANCELLED', 'CANCELLATION_REASON',
+               'AIR_SYSTEM_DELAY', 'SECURITY_DELAY', 'AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY', 'WEATHER_DELAY',
+               'DEPARTURE_TIME', 'DEPARTURE_DELAY', 'TAXI_OUT', 'WHEELS_OFF',
+               'ELAPSED_TIME', 'AIR_TIME', 'WHEELS_ON', 'TAXI_IN']
+DUP_COLS = ['AIRPORT_ORIGIN', 'AIRPORT_DESTINATION']
+EXTRA = ['DATE']
 flights = pd.read_csv("./data/dev/flights.csv", dtype=COLTYPES)
+for airport in AIRPORTS:
+    flights = flights[flights[airport].str.isalpha()]
+
 airports = pd.read_csv("./data/dev/airports.csv")
+airports.loc[airports.IATA_CODE == 'PBG', 'LATITUDE'] = 44.6521
+airports.loc[airports.IATA_CODE == 'PBG', 'LONGITUDE'] = -73.4679
 holidays = pd.read_csv("./data/dev/2015_Public_Holidays.csv")
 
 # Merge data files
 flights = merge_two_airports(flights, airports, ["ORIGIN", "DESTINATION"])
 flights = flights.merge(holidays, how="left", on=["MONTH", "DAY"])
 
-# Define target
+# Define target: flights departed 15 mins or more later than the scheduled departured
 flights['TARGET'] = define_target(flights, 'DEPARTURE_DELAY', 15)
 
 # Create new features
 # flags for holiday, weekend, international airport
 # traffic estimate based on flight counts for date, month, airport, airline
 
-flights = categorize_multiple(flights, CAT_FEATURES)
+
 flights['HOLIDAY_FLAG'] = flights['HOLIDAY'].notnull()*1
 flights['WEEKEND'] = (flights['DAY_OF_WEEK'].isin([6, 7]))*1
 flights['DATE'] = pd.to_datetime(
@@ -36,7 +47,7 @@ for col in ['DATE', 'MONTH', 'AIRLINE']:
     flights = join_aggregates(flights, col, aggregate, col+"_TRAFFIC")
 
 airport_size = flights[AIRPORTS[0]].value_counts(
-) + flights[AIRPORTS[1]].value_counts()
+).add(flights[AIRPORTS[1]].value_counts(), fill_value=0)
 
 for airport in AIRPORTS:
     flights = join_aggregates(
@@ -50,5 +61,19 @@ for coordinate in COORDINATES:
     flights[coordinate+"_DIF"] = flights[coordinate +
                                          '_ORIGIN'] - flights[coordinate+'_DESTINATION']
 
-# print(flights.columns)
+flights.drop(UNAVAILABLE + EXTRA + DUP_COLS, axis=1, inplace=True)
+
+STR_VAR = [v for v in flights.columns if is_string_dtype(flights[v])]
+flights = categorize_multiple(flights, STR_VAR)
+flights.drop(STR_VAR, axis=1, inplace=True)
+
+# NULL_VAR = get_col_with_null(flights)
+# NUM_VAR_COMPLETE = [v for v in flights.columns if is_numeric_dtype(
+#     flights[v]) and v not in [NULL_VAR]]
+# selector = VarianceThreshold()
+# selector.fit(flights[NUM_VAR_COMPLETE])
+# NUM_VAR_REMOVED = [NUM_VAR_COMPLETE[i] for i in range(
+#     len(NUM_VAR_COMPLETE)) if selector.get_support()[i] == False]
+# flights.drop(NUM_VAR_REMOVED, axis=1, inplace=True)
+
 flights.to_csv("./data/dev/flights_processed.csv", index=False)
