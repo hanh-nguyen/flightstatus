5051375777868d299725acddd290a9f8485915d1
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-06-01 08:58:30 -0400

    organize files

--- test/modeling/training.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def search_result(searchcv):
-    result = pd.DataFrame.from_dict(searchcv.cv_results_['params'])
-    result['mean_score'] = searchcv.cv_results_['mean_test_score']
-    result['std_score'] = searchcv.cv_results_['std_test_score']
-    return result.sort_values('mean_score', ascending=False)
