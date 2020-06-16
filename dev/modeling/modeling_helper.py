a3f30b40bb35e2b67b099cac2776b0c3cf1b67a9
hanh-nguyen <myhanh.nguyen1211@gmail.com>
2020-06-15 20:34:04 -0400

    add get_roc and optimize threshold

--- dev/modeling/modeling_helper.py
+++ dev/modeling/modeling_helper.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 from keras.utils.np_utils import to_categorical
+from sklearn.metrics import classification_report, SCORERS, roc_curve, auc
+import matplotlib.pyplot as plt
 
 
 def search_result(searchcv):
@@ -15,3 +18,28 @@ def create_xy(data):
 
 def transform_xy(data):
     return (data.drop(['TARGET'], axis=1)).as_matrix(), to_categorical(data['TARGET'])
+
+
+def get_roc(y_actual, y_pred, plotting=False):
+    fpr, tpr, _ = roc_curve(y_actual, y_pred)
+    roc_auc = auc(fpr, tpr)
+    if plotting:
+        plt.figure()
+        plt.plot(fpr, tpr, color='darkorange', lw=2,
+                 label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.0])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic example')
+        plt.legend(loc="lower right")
+        plt.show()
+    return roc_auc
+
+
+def optimize_threshold(y_actual, y_pred):
+    fpr, tpr, thresholds = roc_curve(y_actual, y_pred)
+    opt_idx = np.argmin(np.sqrt(np.square(1-tpr) + np.square(fpr)))
+    opt_threshold = thresholds[opt_idx]
+    return opt_threshold
